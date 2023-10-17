dc589b69b9bfe70539da9467dd77653de7ee167e
Juan-MZ <juanjosz@unicauca.edu.co>
2023-10-17 15:47:16 -0500

    Revert "Revert "feat: add of new models""
    This reverts commit 7e04aceb89f3b02bc07bc0034bd79c14b28bb1ce.

---
+++ digital-repository-back/src/main/java/co/unicauca/digital/repository/back/domain/model/score/Criteria.java
@@ -0,0 +1,34 @@
+package co.unicauca.digital.repository.back.domain.model.score;
+
+import lombok.*;
+
+import javax.persistence.*;
+
+/**
+ * Class that defines an entity for the O/R mapping of the CRITERIA table.
+ */
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Criteria {
+    /** Criteria Id */
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    /** Criteria Name */
+    @Column(length = 250)
+    private String name;
+
+    /** Criteria Description */
+    @Column(length = 250)
+    private String description;
+
+    /** Criteria ScoreCriteria */
+    @OneToOne(mappedBy = "criteria", optional = false)
+    @JoinColumn(name = "scorecriteriaId")
+    private ScoreCriteria scoreCriteria;
+}
