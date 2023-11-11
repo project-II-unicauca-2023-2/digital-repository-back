335352ecc9249ea5d12c9ec27d865abd23ba56d8
CDN51 <carlosda@unicauca.edu.co>
2023-11-10 23:17:06 -0500

    Actualización repo vendorType

--- digital-repository-back/src/main/java/co/unicauca/digital/repository/back/domain/repository/vendorType/IVendorTypeRepo.java
+++ digital-repository-back/src/main/java/co/unicauca/digital/repository/back/domain/repository/vendorType/IVendorTypeRepo.java
@@ -1,13 +1,11 @@
 package co.unicauca.digital.repository.back.domain.repository.vendorType;
 
-import co.unicauca.digital.repository.back.domain.dto.vendorType.response.vendorTypeDtoGetTypeResponse;
-import co.unicauca.digital.repository.back.domain.dto.vendorType.response.vendorTypeDtoGetTypeSpecResponse;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import co.unicauca.digital.repository.back.domain.dto.aboutVendor.response.aboutVendorDto;
 import co.unicauca.digital.repository.back.domain.model.contractType.ContractType;
 
 
@@ -16,12 +14,9 @@ import co.unicauca.digital.repository.back.domain.model.contractType.ContractTyp
  */ 
 @Repository
 public interface IVendorTypeRepo extends JpaRepository<ContractType, Integer> {
-
-    @Query("SELECT new co.unicauca.digital.repository.back.domain.dto.vendorType.response.vendorTypeDtoGetTypeResponse(v.name) FROM ContractType v WHERE v.externalCode = :code")
-    vendorTypeDtoGetTypeResponse getTypeByMask(@Param("code") String code);
     
-    @Query("SELECT new co.unicauca.digital.repository.back.domain.dto.vendorType.response.vendorTypeDtoGetTypeSpecResponse(v.name, v.description) FROM ContractType v WHERE v.externalCode = :code")
-    vendorTypeDtoGetTypeSpecResponse getTypeAndSpecByMask(@Param("code") String code);
+    @Query("SELECT new co.unicauca.digital.repository.back.domain.dto.aboutVendor.response.aboutVendorDto(v.id, v.description, v.name) FROM ContractType v WHERE v.externalCode = :code")
+    aboutVendorDto getTypeAndSpecByMask(@Param("code") String code);
 
     
 }
