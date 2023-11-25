5707e54ae2d45bbc07c3da15f615b570243eefed
Cristian <cristianenv13@gmail.com>
2023-11-24 22:16:46 -0500

    Fix upload excel files

--- digital-repository-back/src/main/java/co/unicauca/digital/repository/back/domain/repository/score/IScoreRepository.java
+++ digital-repository-back/src/main/java/co/unicauca/digital/repository/back/domain/repository/score/IScoreRepository.java
@@ -1,19 +1,32 @@
 package co.unicauca.digital.repository.back.domain.repository.score;
 
+import co.unicauca.digital.repository.back.domain.model.contract.Contract;
 import co.unicauca.digital.repository.back.domain.model.score.Score;
 
 import java.util.Optional;
 
+import javax.transaction.Transactional;
+
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 /**
  * Repository that manages the persistence of the Score entity in the database.
  */
 @Repository
+@Transactional
 public interface IScoreRepository extends JpaRepository<Score, Integer> {
     /**
      * Query find Score by Id
      */
     Optional<Score> findById(Integer id);
+
+    @Modifying
+    @Query(value = "UPDATE score SET totalScore = :totalScore WHERE contract_id = :contractId", nativeQuery = true)
+    void updateByContractId(Float totalScore, Integer contractId);
+
+    Optional<Score> findByContract(Contract contract);
 }
--- digital-repository-back/src/main/java/co/unicauca/digital/repository/back/domain/service/scan/impl/ScanFileServiceImpl.java
+++ digital-repository-back/src/main/java/co/unicauca/digital/repository/back/domain/service/scan/impl/ScanFileServiceImpl.java
@@ -8,6 +8,7 @@ import java.time.LocalDateTime;
 import static java.time.LocalDateTime.now;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 
 import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.FormulaEvaluator;
@@ -18,6 +19,7 @@ import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
+import co.unicauca.digital.repository.back.domain.model.contract.Contract;
 import co.unicauca.digital.repository.back.domain.model.score.Score;
 import co.unicauca.digital.repository.back.domain.model.scoreCriteria.ScoreCriteria;
 import co.unicauca.digital.repository.back.domain.repository.contract.IContractRepository;
@@ -86,37 +88,37 @@ public class ScanFileServiceImpl implements IScanFileService {
         final Cell cell9C = sheet.getRow(8).getCell(NUM_COLUMN_C);
         vendorName = cell9C.toString();
         System.out.println("Vendor name:" + vendorName);
-        
+
         // Identification type cell
         final Cell cell9H = sheet.getRow(8).getCell(NUM_COLUMN_H);
         vendorIdentificationType = cell9H.toString();
         System.out.println("Identification type:" + vendorIdentificationType);
-        
+
         // Vendor cc,nit O identification cell
         final Cell cell9J = sheet.getRow(8).getCell(NUM_COLUMN_J);
         vendorIdentification = excelUtils.extractIntegerValue(cell9J.toString()).toString();
         System.out.println("Vendor Identification: " + vendorIdentification);
-        
+
         // Initial date cell
         final Cell cell10C = sheet.getRow(9).getCell(NUM_COLUMN_C);
         if (cell10C.getLocalDateTimeCellValue() != null) {
             initialDate = cell10C.getLocalDateTimeCellValue();
             System.out.println("Initial date: " + initialDate.toString());
         }
-        
+
         // Final date cell
         final Cell cell10I = sheet.getRow(9).getCell(NUM_COLUMN_I);
         if (cell10I.getLocalDateTimeCellValue() != null) {
             finalDate = cell10I.getLocalDateTimeCellValue();
             System.out.println("Final Date: " + finalDate.toString());
         }
-        
+
         // Contract subject cell
         final Cell cell11A = sheet.getRow(10).getCell(NUM_COLUMN_A);
         contractSubject = excelUtils.extractContractSubject(cell11A.toString());
         System.out.println("Contract Subject: " + contractSubject);
         System.out.println("==== Vendor type ====");
-        
+
         // Vendor type cells
         // Goods
         final Cell cell16J = sheet.getRow(15).getCell(NUM_COLUMN_J);
@@ -132,14 +134,14 @@ public class ScanFileServiceImpl implements IScanFileService {
         final Cell cell25J = sheet.getRow(24).getCell(NUM_COLUMN_J);
         // Works
         final Cell cell26J = sheet.getRow(25).getCell(NUM_COLUMN_J);
-        
+
         // Determine the type of vendor to evaluate
         vendorTypes = Arrays.asList(cell16J.toString(), cell17J.toString(), cell18J.toString(),
                 cell19J.toString(), cell20J.toString(), cell21J.toString(), cell22J.toString(),
                 cell23J.toString(), cell24J.toString(), cell25J.toString(), cell26J.toString());
         criteriaType = excelUtils.determineVendorType(vendorTypes);
         System.out.println("El criteria type es: " + criteriaType);
-        
+
         System.out.println("==== Score ====");
         // First Criteria (Quality) scoring cells
         final Cell cell45A = sheet.getRow(44).getCell(NUM_COLUMN_A);
@@ -159,13 +161,14 @@ public class ScanFileServiceImpl implements IScanFileService {
         System.out.println(firstCriteriaType + ": " + qualityCriteriaRate + "\n" +
                 secondCriteria + ": " + complianceCriteriaRate + "\n" +
                 thirdCriteria + ": " + excecutionCriteriaRate + "\n");
-        
+
         // Total evaluation cells
         final Cell cell47J = sheet.getRow(46).getCell(NUM_COLUMN_J);
         totalScore = excelUtils.evaluateFormula(cell47J, evaluator);
         System.out.println("Total score: " + totalScore + "\n");
-        
-        if (workbook != null) workbook.close();
+
+        if (workbook != null)
+            workbook.close();
     }
 
     @Override
@@ -182,32 +185,38 @@ public class ScanFileServiceImpl implements IScanFileService {
         var complianceCriteria = this.criteriaRepository.findByNameAndCriteriaType("Cumplimiento", criteriaType)
                 .orElseThrow();
 
-        Score score = Score.builder()
-                .totalScore(totalScore)
-                .contract(contract)
-                .createTime(now())
-                .build();
-        this.scoreRepository.save(score);
+        Optional<Contract> contractOptional = contractRepository.findByReference(contractReference);
+        scoreRepository.updateByContractId(totalScore, contractOptional.get().getId());
+
+        Optional<Score> scoreOptional = scoreRepository.findByContract(contractOptional.get());
+
+        // Score score = Score.builder()
+        // .totalScore(totalScore)
+        // .contract(contractOptional.get())
+        // .createTime(now())
+        // .build();
+        // this.scoreRepository.save(score);
 
         ScoreCriteria firstScoreCriteria = ScoreCriteria.builder()
-                .score(score)
+                .score(scoreOptional.get())
                 .criteria(qualityCriteria)
                 .rate(qualityCriteriaRate)
                 .createTime(now())
                 .build();
         ScoreCriteria secondScoreCriteria = ScoreCriteria.builder()
-                .score(score)
+                .score(scoreOptional.get())
                 .criteria(complianceCriteria)
                 .rate(complianceCriteriaRate)
                 .createTime(now())
                 .build();
         ScoreCriteria thirdScoreCriteria = ScoreCriteria.builder()
-                .score(score)
+                .score(scoreOptional.get())
                 .criteria(executionCriteria)
                 .rate(excecutionCriteriaRate)
                 .createTime(now())
                 .build();
-        this.scoreCriteriaRepository.saveAll(List.of(firstScoreCriteria, secondScoreCriteria, thirdScoreCriteria));
+        this.scoreCriteriaRepository.saveAll(List.of(firstScoreCriteria,
+                secondScoreCriteria, thirdScoreCriteria));
         cleanData();
     }
 
@@ -235,7 +244,7 @@ public class ScanFileServiceImpl implements IScanFileService {
         InputStream is = file.getInputStream();
         Workbook workbook = new XSSFWorkbook(is);
         Sheet sheet = workbook.getSheetAt(0);
-        
+
         Row row; // cada fila del excel
         int rownum = 1; // empieza en la fila 2
         boolean isRowEmpty = false; // para saber si la fila esta vacia
@@ -245,94 +254,110 @@ public class ScanFileServiceImpl implements IScanFileService {
         final String CE = "4 CÉDULA DE EXTRANJERÍA";
 
         System.out.println("\n============= Data Massive File ==============\n");
-        while(!isRowEmpty) { // mientras la fila no este vacia
+        while (!isRowEmpty) { // mientras la fila no este vacia
             row = sheet.getRow(rownum); // se obtiene la fila
             contractReference = row.getCell(NUM_COLUMN_A).toString();
-            if( contractReference.isBlank() ) {
+            if (contractReference.isBlank()) {
                 isRowEmpty = true;
             }
             // Comprobar si la celda en la coulmna L no es nula y no es NA
-            if(!isRowEmpty && row.getCell(NUM_COLUMN_L)!=null && !row.getCell(NUM_COLUMN_L).toString().equals("NA")) {
+            if (!isRowEmpty && row.getCell(NUM_COLUMN_L) != null
+                    && !row.getCell(NUM_COLUMN_L).toString().equals("NA")) {
                 // Comprobar si existe el contrato
                 var contract = contractRepository.findByReference(contractReference);
-                if(contract.isPresent()) {
+                if (contract.isPresent()) {
                     System.out.println("Printing row " + (rownum + 1) + "\n");
 
                     System.out.println("Reference: " + contractReference);
-                    
+
                     LocalDateTime suscriptionDate = row.getCell(NUM_COLUMN_B).getLocalDateTimeCellValue();
-                    if(suscriptionDate!=null) System.out.println("Susciption date: " + suscriptionDate);
-                    
+                    if (suscriptionDate != null)
+                        System.out.println("Susciption date: " + suscriptionDate);
+
                     String contractType = row.getCell(NUM_COLUMN_C).toString();
-                    if(!contractType.isBlank()) System.out.println("Contract type: " + contractType);
-                    
+                    if (!contractType.isBlank())
+                        System.out.println("Contract type: " + contractType);
+
                     String contractSubject = row.getCell(NUM_COLUMN_D).toString();
-                    if(!contractSubject.isBlank()) System.out.println("Contract subject: " + contractSubject);
-                    
+                    if (!contractSubject.isBlank())
+                        System.out.println("Contract subject: " + contractSubject);
+
                     String identificationType = row.getCell(NUM_COLUMN_E).toString();
-                    if(!identificationType.isBlank()) System.out.println("Identification type: " + identificationType);
-                    
-                    String identificationNumber="";
+                    if (!identificationType.isBlank())
+                        System.out.println("Identification type: " + identificationType);
+
+                    String identificationNumber = "";
                     // Comprobar si es persona natural
-                    if(identificationType.equals(RUT) || identificationType.equals(CC) || identificationType.equals(CE)) {
+                    if (identificationType.equals(RUT) || identificationType.equals(CC)
+                            || identificationType.equals(CE)) {
                         isNaturalPerson = true;
                     }
                     // Comprobar que, si es persona natural,
                     // que la celda en la columna F no sea nula
                     var cellF = row.getCell(NUM_COLUMN_F).toString();
-                    if(isNaturalPerson == true && !cellF.isBlank()) {
-                        if(!(row.getCell(NUM_COLUMN_G).toString()).isBlank()) {
+                    if (isNaturalPerson == true && !cellF.isBlank()) {
+                        if (!(row.getCell(NUM_COLUMN_G).toString()).isBlank()) {
                             System.out.println("Error: NIT is not blank");
                         } else {
                             identificationNumber = row.getCell(NUM_COLUMN_F).toString();
                         }
                     }
-                    // Comprobar que, si no es persona natural, 
+                    // Comprobar que, si no es persona natural,
                     // que la celda en la columna G no sea nula
                     var cellG = row.getCell(NUM_COLUMN_G).toString();
-                    if(isNaturalPerson == false && !cellG.isBlank()) {
-                        if(!(row.getCell(NUM_COLUMN_F).toString()).isBlank()) {
+                    if (isNaturalPerson == false && !cellG.isBlank()) {
+                        if (!(row.getCell(NUM_COLUMN_F).toString()).isBlank()) {
                             System.out.println("Error: CC/RUT is not blank");
                         } else {
                             identificationNumber = row.getCell(NUM_COLUMN_G).toString();
                         }
                     }
-                    if(!identificationNumber.isBlank()) System.out.println("Identification number: " + identificationNumber);
-                    
+                    if (!identificationNumber.isBlank())
+                        System.out.println("Identification number: " + identificationNumber);
+
                     String vendorName = row.getCell(NUM_COLUMN_H).toString();
-                    if(!vendorName.isBlank()) System.out.println("Vendor name: " + vendorName);
-                    
+                    if (!vendorName.isBlank())
+                        System.out.println("Vendor name: " + vendorName);
+
                     String supervisorName = row.getCell(NUM_COLUMN_I).toString();
-                    if(!supervisorName.isBlank()) System.out.println("Supervisor name: " + supervisorName);
-                    
+                    if (!supervisorName.isBlank())
+                        System.out.println("Supervisor name: " + supervisorName);
+
                     LocalDateTime initialDate = row.getCell(NUM_COLUMN_J).getLocalDateTimeCellValue();
-                    if(initialDate!=null) System.out.println("Initial date: " + initialDate);
-                    
+                    if (initialDate != null)
+                        System.out.println("Initial date: " + initialDate);
+
                     LocalDateTime finalDate = row.getCell(NUM_COLUMN_K).getLocalDateTimeCellValue();
-                    if(finalDate!=null) System.out.println("Final date: " + finalDate);
-                    
+                    if (finalDate != null)
+                        System.out.println("Final date: " + finalDate);
+
                     Double contractEvaluation = row.getCell(NUM_COLUMN_L).getNumericCellValue();
                     totalScore = contractEvaluation.floatValue();
                     System.out.println("Contract evaluation: " + totalScore + "\n");
 
                     // Guardar en la base de datos
-                    Score score = Score.builder()
-                        .totalScore(totalScore)
-                        .contract(contract.get())
-                        .createTime(now())
-                        .build();
-                    this.scoreRepository.save(score);
+
+                    Optional<Contract> contractOptional = contractRepository.findByReference(contractReference);
+                    scoreRepository.updateByContractId(totalScore, contractOptional.get().getId());
+
+                    // Score score = Score.builder()
+                    // .totalScore(totalScore)
+                    // .contract(contract.get())
+                    // .createTime(now())
+                    // .build();
+                    // this.scoreRepository.save(score);
                 }
             }
             rownum++;
         }
         System.out.println("================= End =====================");
-        if (workbook != null) workbook.close();
+        if (workbook != null)
+            workbook.close();
         cleanData();
     }
 
     @Override
     public void saveMassiveData() {
-        
+
     }
 }
