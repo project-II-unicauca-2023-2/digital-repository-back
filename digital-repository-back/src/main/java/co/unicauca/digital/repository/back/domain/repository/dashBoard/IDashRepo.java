609dcaebd9f0756cfdd5d1c955a84e776e1e88c2
CDN51 <carlosda@unicauca.edu.co>
2023-12-04 22:08:22 -0500

    Actualización obtención datos calificacion

--- digital-repository-back/src/main/java/co/unicauca/digital/repository/back/domain/repository/dashBoard/IDashRepo.java
+++ digital-repository-back/src/main/java/co/unicauca/digital/repository/back/domain/repository/dashBoard/IDashRepo.java
@@ -22,6 +22,15 @@ public interface IDashRepo extends JpaRepository<Contract, Integer>{
             "where ct.description=:desc and s.totalScore>=:min and s.totalScore<:max and year(s.createTime)=:year")
     List<DataSave> getDataByDes(@Param("desc") String desc, @Param("min") Float min, @Param("max") Float max, @Param("year") Integer year);
 
+    @Query("select new co.unicauca.digital.repository.back.domain.dto.dashboard.Internal.DataSave(c.id, m.contractType.id, ct.name) "+
+            "from "+
+            "Contract c inner join Score s on c.id=s.contract.id "+
+            "inner join ModalityContractType m on c.modalityContractType.id = m.id "+
+            "inner join ContractType ct on m.contractType.id=ct.id "+
+            "where s.createTime=s.updateTime")
+    List<DataSave> getDataByDesNoScore(@Param("desc") String desc, @Param("year") Integer year);
+
+
     
     
 }
